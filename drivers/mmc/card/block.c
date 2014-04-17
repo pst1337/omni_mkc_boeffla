81dcb6c4460bdd284a891184aa3b7559c4ab4280
pst1337 <pst1337@gmail.com>
2014-04-17 13:59:04 +0200

    module.c: kernel: make an exception in module loading to load proprietary modules

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2530,8 +2530,7 @@ static int mmc_blk_probe(struct mmc_card *card)
 	printk(KERN_INFO "%s: %s %s %s %s\n",
 		md->disk->disk_name, mmc_card_id(card), mmc_card_name(card),
 		cap_str, md->read_only ? "(ro)" : "");
-<<<<<<< HEAD
-=======
+
 
 	if (mmc_blk_alloc_parts(card, md))
 		goto out;
@@ -2540,7 +2539,6 @@ static int mmc_blk_probe(struct mmc_card *card)
 	if (mmc_blk_alloc_parts(card, md))
 		goto out;
 #endif
->>>>>>> 722b6da... block.c: allocate boot (MMC) partitions on probe
 
 	mmc_set_drvdata(card, md);
 	mmc_fixup_device(card, blk_fixups);
